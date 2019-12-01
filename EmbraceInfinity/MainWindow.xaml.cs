6738cc48c321b2f565887895e51ee5427e3d29fb
Shuur1k <viktor65dav@mail.ru>
2019-12-01 18:25:24 +0300

    Реализована форма входа и два окна для администратора и для пользователя

--- EmbraceInfinity/MainWindow.xaml.cs
+++ EmbraceInfinity/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace EmbraceInfinity
 {
@@ -23,6 +24,42 @@ namespace EmbraceInfinity
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LogInClick(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}");
+
+            if (regex.IsMatch(TextBoxPassword.Password))
+            {
+                EMTYEntities db = new EMTYEntities();
+                var user = db.User.AsNoTracking().FirstOrDefault(u => u.Login == TextBoxLigin.Text && u.Password == TextBoxPassword.Password);
+                    if (user == null)
+                    {
+                        LabelAnswer.Content = "Пользователь не найден";
+                    }
+                    else
+                    {
+                        LabelAnswer.Content = "Пользователь найден";
+                        WindowControl windowControl = new WindowControl();
+                        WindowUser windowUser = new WindowUser();
+                        if (user.Login == "Admin")
+                        {
+                            Hide();
+                            windowControl.Show();
+                        }
+                        else
+                        {
+                            Hide();
+                            windowUser.Show();
+                        }
+                    }
+            }
+            else if (string.IsNullOrEmpty(TextBoxLigin.Text) || string.IsNullOrEmpty(TextBoxPassword.Password))
+            {
+                LabelAnswer.Content = "Введите логин или пароль";
+            }
+            else LabelAnswer.Content = "Неправильно введёт логин или пароль";
         }
     }
 }
---
+++ EmbraceInfinity/WindowsUser.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmbraceInfinity
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowControl.xaml
+    /// </summary>
+    public partial class WindowUser : Window
+    {
+        public WindowUser()
+        {
+           InitializeComponent();
+        }
+
+        private void ButtonFeacher_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCoursorMenu(index);
+
+            switch (index)
+            {
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlInicio());
+                    break;
+                //case 1:
+                //    GridPrincipal.Children.Clear();
+                //    GridPrincipal.Children.Add(new UserControlClients());
+                //    break;
+                default:
+                    break;
+
+            }
+        }
+        private void MoveCoursorMenu(int index)
+        {
+            TransitioningContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, (70 + (60 * index)), 0, 0);
+
+        }
+    }
+}
